636bb50a1cdf30fe81eaf7042d688fce5603c2dc
poojaa09 <poojarawat0329@gmail.com>
2025-05-18 00:46:30 +0530

    navbar and sidebar done with animation

---
+++ store/src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Navbar from './components/Navbar/Navbar'
+import { UpdateFollower } from 'react-mouse-follower'
+const App = () => {
+  return (
+    <main className='overflow-x-hidden'>
+       <UpdateFollower
+      mouseOptions={{
+        backgroundColor: 'white',
+        zIndex: 999,
+        followSpeed: 1.5,
+      }}
+    >
+      <Navbar/>
+      </UpdateFollower>
+   </main>
+  )
+}
+
+export default App
